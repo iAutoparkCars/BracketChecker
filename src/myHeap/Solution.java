ff9f04dbbd72ddfdf66365367fd96c64eaa02404
Steven <bonillasc@gmail.com>
2017-06-01 16:19:13 -0400

    modified insert to be more re-useable, and now can delete specified values by calling insert to swap the element to delete and last element

--- src/myHeap/Solution.java
+++ src/myHeap/Solution.java
@@ -24,7 +24,7 @@ public class Solution
             switch(a[0])
             {
                 case "1":
-                    insert(Integer.valueOf(a[1]));
+                    insert(heap.size(),Integer.valueOf(a[1]));
                     break;
                 case "2":
                     delete(Integer.valueOf(a[1]));
@@ -47,43 +47,111 @@ public class Solution
     //Delete element. Hard. Must find...delete, then rebalance heap
     //Print the minimum. Easy So this will be a min heap.
     
-    
-    public static void insert(int v)
+    //inserting at heap.size() is like inserting at last spot that doesn't exist yet  
+    public static void insert(int index, int v)
     {
        //if heap is size 2, only placeholder & root, so don't check
-       heap.add(v);
-        
-       //if the parent is larger than the child, percolate
-       int currInd = heap.size()-1;
-       if (heap.size() > 2 && parentVal(currInd) > heap.get(currInd))
+       if (index>heap.size()-1)
        {
-         //System.out.println(heap.size()-1);
-         System.out.println("parent of " + v + " is " + parentVal(currInd) + 
-                           " so I want to swap");
-           
-         percolate(currInd,parentIndex(currInd));  //child index, parent index
-         //check here
+           heap.add(-5);
+           heap.set(index,v); 
        }
-
+        else 
+        {heap.set(index,v);}
+        
+        //if the parent is larger than the current, percolate
+       if (heap.size() > 2 && parentIndex(index)>2 && parentVal(index) > heap.get(index))
+       {
+           percolateDown(index,parentIndex(index));  //child index, parent index
+       }
+        
+        //if child is smaller than current, percolate
+       
+       
+      if (heap.size() > 2 && l_childIndex(index) < heap.size() && (l_childVal(index) < heap.get(index)))
+       {
+         percolateUp(index,l_childIndex(index),"left");  //left child index, parent index
+       }
+        
+        if (heap.size() > 3 && r_childIndex(index) < heap.size() && (r_childVal(index) < heap.get(index)))
+       {
+           percolateUp(index,l_childIndex(index),"right");  //right child index, parent index
+       } 
+        
+      
+        
+        
+        
+        
         //System.out.println("1 " + v); 
     }
     
-    public static void percolate(int child, int parent)
+    public static void percolateDown(int curr, int parent)
     {
         if (heap.get(parent)==null)
             return;
         
-        int temp = heap.get(child);
-        heap.set(child,heap.get(parent));
+        int temp = heap.get(curr);
+        heap.set(curr,heap.get(parent));
         heap.set(parent,temp);
         
         //percolate the next level higher (new child, new parent)
-        percolate(parent,parentIndex(parent));
+        percolateDown(parent,parentIndex(parent));
     }
-                           
+    
+    public static void percolateUp(int curr, int child, String which)
+    {
+        if (heap.get(child)==null || child >= heap.size() || curr >= heap.size())
+            return;
+        
+        int temp = heap.get(child);
+        heap.set(child,heap.get(curr));
+        heap.set(curr,temp);
+       
+        
+        //percolate the next level higher (new child, new parent)
+        if (which.equals("left") && l_childIndex(child) < heap.size())
+        {
+            percolateUp(child,l_childIndex(child),"left");
+        }
+        else if (which.equals("right") && r_childIndex(child) < heap.size())
+        {
+            percolateUp(child,r_childIndex(child),"right");
+        }
+    }
+    
     public static void delete(int v)
     {
-        //System.out.println("2 " + v); 
+        Integer targInd = -1;
+        int last_i = heap.size()-1;
+        for (int i =1 ; i < heap.size(); i++)
+        {
+            if (heap.get(i).equals(v))
+            {
+                targInd = i;
+            }
+        }
+        if(targInd.equals(-1))
+            {return; } //value not found
+        
+        if (targInd.equals(last_i))
+        {
+            heap.remove(last_i);
+            return;
+        }
+        
+        //placing last element into element you want to delete
+        int valTmp = heap.get(last_i);
+        
+       //System.out.println("delete " + v + " found at i=" + targInd + " by replacing it with value " +valTmp);
+        
+        heap.remove(last_i);
+        
+        //for (Integer i : heap)
+        //System.out.print(i + " " );
+        
+        
+        insert(targInd,valTmp);
     }   
                            
     public static void printMin()
@@ -91,19 +159,19 @@ public class Solution
         //System.out.println("3 ");
     }
     
-     public static int parentVal(int i)
+    public static int parentVal(int i)
     {
         return heap.get(parentIndex(i));
     }
     
-    public static int l_childVal()
+    public static int l_childVal(int i)
     {
-        return heap.get(l_childIndex());
+        return heap.get(l_childIndex(i));
     }
      
-    public static int r_childVal()
+    public static int r_childVal(int i)
     {
-        return heap.get(r_childIndex());
+        return heap.get(r_childIndex(i));
     }
     
     
@@ -117,15 +185,15 @@ public class Solution
         return parentInd;
     }
     
-    public static int l_childIndex()
+    public static int l_childIndex(int i)
     {
-        int i = heap.size()-1;
         return 2*i;
     }
     
-    public static int r_childIndex()
+    public static int r_childIndex(int i)
     {
-        int i = heap.size()-1; //childIndex
         return (2*i)+1;
     }
+    
+    
 }

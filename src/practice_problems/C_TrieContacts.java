e9e00388fa19b3295f75249594043e2dbaea8ccb
Steven <bonillasc@gmail.com>
2017-07-23 21:01:37 -0400

    mostly done in Contacts. need to find way to 'add' a String that already exists.

--- src/practice_problems/C_TrieContacts.java
+++ src/practice_problems/C_TrieContacts.java
@@ -8,20 +8,57 @@ public class C_TrieContacts
 	public static void main(String[] args)
 	{
 		Trie tree = new Trie();
-		tree.add("hello");
-		tree.traverse("hello");
+		tree.add("monday");
+		tree.add("money");
+		tree.add("monetize");
+		tree.add("mon");
+		
+		tree.find("mon");
+		
+		//tree.add("hack");
+		//tree.add("hackerrank");
+		//tree.find("hac");
+		
+		
+		
+		//tree.find("hak");
+		
+		
+		
+		//tree.find("mond");
 	}
 
+	public static int countLeaves(Node root)
+	{
+		int leaves = 0;
+		
+		//base case
+		if (root.children.size()==0)
+			leaves++;
+		else
+		{
+			for (Node child : root.children.values())
+			{
+				leaves = leaves + countLeaves(child);
+			}
+		}
+		
+		return leaves;
+	}
 
 	public static class Node
 	{
 		private Map<String,Node> children;
-		public Node() {children = new HashMap<String, Node>();}
+		private String value;
+		public Node(String val) {children = new HashMap<String, Node>(); this.value = val;}
 		
 		public void put(String letter, Node nxtNode)
 		{
 			children.put(letter, nxtNode);
 		}
+		
+		@Override
+		public String toString() {return value;}
 		
 		public boolean hasNext() {return !children.isEmpty();}
 	}
@@ -29,7 +66,8 @@ public class C_TrieContacts
 	public static class Trie
 	{
 		public Node root;
-		public Trie() {root = new Node();}
+		private int redundantCounter;
+		public Trie() {root = new Node(null);}
 		
 		public void add(String str)
 		{
@@ -41,15 +79,25 @@ public class C_TrieContacts
 			
 			for (char ch : str.toCharArray())
 			{
-				current.children.put(String.valueOf(ch), new Node());
 				
-				
-				current = current.children.get(String.valueOf(ch));
+				if (current.children.containsKey(String.valueOf(ch)))
+				{
+					current = current.children.get(String.valueOf(ch));
+				}
+				else //if not in children, add it
+				{
+					current.children.put(String.valueOf(ch), new Node(String.valueOf(ch)));
+					current = current.children.get(String.valueOf(ch));
+				}
 				if (current==null) {System.out.println("current is null."); return;}
 			}
 			
+			
+			
 			//the first current becomes the new root of the tree
 			root = newRoot;
+			
+			//check here if the tree is the same as before. If "added" and tree is same, then add to counter
 		}
 		
 		public void traverse(String str)
@@ -60,16 +108,65 @@ public class C_TrieContacts
 			{
 				if (current.children.containsKey(String.valueOf(ch)))
 				{
-					System.out.println(ch + " ");
+					System.out.print(ch + " ");
 				}
-				else{System.out.println("Reached end of the tree");	return;}
+				else{System.out.println("|"+ch+"| is not in the tree.");	return;}
 				
 				current = current.children.get(String.valueOf(ch));
 			}
+			System.out.println("");
 		}
+	
+		public void find(String str)
+		{
+			String substr = "";
+			int substrCounter = 0;
+			
+			String tempStr = "";
+			
+			Node current = root;
+			
+			for (char ch : str.toCharArray())
+			{
+				if (!current.children.containsKey(String.valueOf(ch)))
+				{
+					System.out.println(str+" is not in the tree");
+					return;
+				}
+				current = current.children.get(String.valueOf(ch));
+				
+				tempStr = tempStr + ch;
+			}
+			
+			
+			
+			//start counting the leaves here
+			
+			System.out.println("Current node is " + current);
+			System.out.println("Number of leaves is: " + countLeaves(current) + substrCounter );
+		}
+	
+		
+		
 	}
 	
-	
+	public static class Test
+	{
+		//correctly returns 3
+		public void case1(){
+			Trie tree = new Trie();
+			tree.add("nd");
+			tree.add("ne");
+			tree.add("nxf");
+			tree.find("n");
+		}
+		
+		//correctly says that n is not in the tree
+		public void case2() {
+			Trie tree = new Trie();
+			tree.find("n");
+		}
+	}
 }
 
 

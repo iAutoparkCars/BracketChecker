26cce4cf86b42c4cf91bb90549d2d17c961a2e68
Steven <bonillasc@gmail.com>
2017-08-13 12:37:21 -0400

    started problem to find # of max points on a line

---
+++ src/practice_problems/G_MaxPoints.java
@@ -0,0 +1,51 @@
+package practice_problems;
+
+
+
+public class G_MaxPoints {
+	
+	public static void main(String[] args){
+		System.out.println("HI");
+	}
+	
+	/**
+	 * Definition for a point.
+	 * class Point {
+	 *     int x;
+	 *     int y;
+	 *     Point() { x = 0; y = 0; }
+	 *     Point(int a, int b) { x = a; y = b; }
+	 * }
+	 */
+	
+	
+	public static class Solution {
+	    public int maxPoints(Point[] points) {
+	    	/*
+	    	 *  Iterate through the x,y coordinates, hashing each with its respective frequency (value)
+	    	 *  
+	    	 *  Loop through both hashmaps and see which has highest frequency
+	    	 *   	--this will be the line with the most number of points
+	    	 */
+	    	
+	    	return 0;
+	    }
+	}
+	
+	public static class Point{
+		public int x;
+		public int y;
+		public Point(){
+			x = y = 0;
+		}
+		
+		public Point(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+	
+	
+	
+}

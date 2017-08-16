52d89c7793a7af32f599c105a47d53cb7c6b4908
Steven <bonillasc@gmail.com>
2017-08-15 21:45:56 -0400

    started string concat

---
+++ src/practice_problems/I_SubstrConcat.java
@@ -0,0 +1,16 @@
+package practice_problems;
+
+import java.util.List;
+
+public class I_SubstrConcat {
+
+	public static void main(String[] args){
+		Solution solution = new Solution();
+	}
+	
+	public static class Solution {
+	    public List<Integer> findSubstring(String s, String[] words) {
+	        return null;
+	    }
+	}
+}

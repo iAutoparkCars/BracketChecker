101e373205d6f861eda2c70cce7da0d67bc93318
Steven <bonillasc@gmail.com>
2017-09-17 22:06:25 -0400

    fixed bugs with the Chain traversal

--- src/myHashTable/MyHashTable.java
+++ src/myHashTable/MyHashTable.java
@@ -63,68 +63,18 @@ public class MyHashTable {
 			// case 1: overwrite value with existing key
 		if (object.searchReplaceValueSuccess(key, value)){
 			
-			System.out.println("Value with key: " + key + " was replaced.");
+			System.out.println("key: " + key + " exists. Its value was replaced with value: " + value);
 			
 		} 
 			// case 2: Key not found. Two different keys hashed to same index. append to end
 		else {
-				
 				object.addEnd( new Node(key, value) );
 				System.out.println("No key found, but different keys hashed to same index. Value appended to chain.");
 		
 		}
 		
-		
-		/*
-			// if a chain exists, then append a new Node to end of chain
-		if (object instanceof MyLinkedList){
-			System.out.println("Collision. Chain already exists; append value to end.");
-			
-				// add new Node to end of the chain
-			((MyLinkedList) object).addEnd(new Node(key, value));
-		}
-			// chain is not created, create the first Node
-		else {
-			
-			 
-			 * Replacing a key with a new value. 
-			 * 
-			 * Problem: There is a value here. Do I create chain, or replace value?  
-			 * 
-			 * the value you are re-setting is a result of trying to store
-			 * a different value with same key, or is it a collision?
-			 * 
-			
-			System.out.println("Create a chain now. First value in chain.");
-			
-				// create new chain; add value
-			MyLinkedList chain = new MyLinkedList();
-			chain.addEnd(new Node(key, value));
-			
-				// this will be the resulting array
-			Object[] result = new Object[this.array.length];
-			
-				// copy original array into result
-			for (int i = 0; i < this.array.length; i++){
-				result[i] = this.array[i];
-			}
-			
-				// now I can store a MyLinkedList object
-			result[hashIndex] = chain;
-			this.array = result;
-		}
-		*/
-		
-			
 	}
 	/*
-	 * 	Problem: deleting. What if you delete an entry with collisions?
-	 * 		Then the whole chain would be gone, the value of another key will be removed.
-	 * 
-	 *  To make sure you want to delete the item associated with a key, make the array
-	 *  Store Nodes that have the String key as a marker
-	 * 	
-	 * 
 	 * 
 	 *  If a key already exists, must delete the old key, then re-insert.
 	 *  
@@ -160,7 +110,7 @@ public class MyHashTable {
 	
 	public static void main(String[] args){
 		
-		MyHashTable table = new MyHashTable(6);
+		MyHashTable table = new MyHashTable(2);
 		
 		
 		table.set("Hello",  new String("Object1"));
@@ -169,34 +119,17 @@ public class MyHashTable {
 		
 		
 		table.set("Key3",  new String("Object4"));
+		table.set("Test1",  new String("Object5"));
 		
 		
-		//table.displayTable();
+		/*
+		 * Now to simulate two different keys hashing to same place in the hashTable
+		*/
 		
-		//table.set("Hello",  new String("Object4"));
+		table.displayTable();
 		
-		
-		//printArray(table.array);
 	}
 	
-	/*public static boolean isPrime(int number){ 
-		
-		boolean isPrime = true;
-		int temp;
-		
-		for(int i=2;i<=number/2;i++)
-		{
-	           temp=number%i;
-		   if(temp==0)
-		   {
-		      isPrime=false;
-		      break;
-		   }
-		}
-		
-		return isPrime;
-	}*/
-
 	// if size is prime, use size. If not, get next highest prime for array size. 
 	private int getNextPrime(int number, int search_cap){
 		
@@ -324,9 +257,8 @@ public class MyHashTable {
 			StringBuilder str = new StringBuilder();
 
 			Node i = head;
-			str.append("-> "+i);
 			
-			while (i.next != null){
+			while (i != null){
 				
 				str.append("-> "+i);
 				i = i.next;
